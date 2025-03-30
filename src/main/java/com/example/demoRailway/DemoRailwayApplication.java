5e83d95c1908ba18412c46d5a2d036cceb90bf44
Ganesh <ganeshvasireddi456@gmail.com>
2025-03-30 14:23:11 +0530

    removed unwanted code

--- src/main/java/com/example/demoRailway/DemoRailwayApplication.java
+++ src/main/java/com/example/demoRailway/DemoRailwayApplication.java
@@ -2,9 +2,6 @@ package com.example.demoRailway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
 @SpringBootApplication
 public class DemoRailwayApplication {
 
@@ -12,8 +9,4 @@ public class DemoRailwayApplication {
 		SpringApplication.run(DemoRailwayApplication.class, args);
 	}
 
-	@GetMapping("/")
-    public String hello() {
-      return String.format("Hello world from Java Spring Boot!");
-    }
 }
