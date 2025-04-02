83e034227fa795e0fbc296039643cf2501b4c3a0
Ganesh <ganeshvasireddi456@gmail.com>
2025-04-02 14:17:30 +0530

    Added cors fix

--- src/main/java/com/example/demoRailway/DemoRailwayApplication.java
+++ src/main/java/com/example/demoRailway/DemoRailwayApplication.java
@@ -2,11 +2,26 @@ package com.example.demoRailway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class DemoRailwayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoRailwayApplication.class, args);
 	}
-
+ @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                System.out.println("✅ CORS Config Applied!");
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
